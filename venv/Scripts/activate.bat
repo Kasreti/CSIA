5de47c72f82e8a95e41f01b9c68f6911789ecd8b
Kasreti <chegwin1215@gmail.com>
2024-01-08 09:54:58 +0800

    changed config stuff so that it works on laptop

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\School\csia_files\venv
+set VIRTUAL_ENV=C:\Users\User\Documents\GitHub\CSIA\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
